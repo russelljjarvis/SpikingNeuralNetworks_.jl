8e5f618cf27870abe62d4b4c41c3574952c4f9c9
Russell Jarvis <russelljarvis@protonmail.com>
2021-12-29 20:31:05 +1100

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -6,7 +6,6 @@ using TOML
 Pkg.add("PyPlot")
 Pkg.add(url = "https://github.com/russelljjarvis/SpikeSynchrony.jl")
 Pkg.add(url = "https://github.com/paulmthompson/SpikeSorting.jl.git")
-Pkg.add(url = "https://github.com/paulmthompson/Spikes.jl.git")
 Pkg.add("Conda")
 Pkg.add("PyCall")
 Pkg.add("SharedArrays")
