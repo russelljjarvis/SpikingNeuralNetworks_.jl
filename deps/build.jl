2ec1ec96fb5ded12bdbfb30fabb186baedcb6bef
Russell Jarvis <russelljarvis@protonmail.com>
2021-12-29 19:16:30 +1100

    update all

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,11 @@
 using Pkg
 using TOML
-Pkg.add("PyPlot")
 Pkg.add(url = "https://github.com/russelljjarvis/SpikeSynchrony.jl")
 Pkg.add(url = "https://github.com/paulmthompson/SpikeSorting.jl.git")
 Pkg.add(url = "https://github.com/paulmthompson/Spikes.jl.git")
 Pkg.add("Conda")
 Pkg.add("PyCall")
+Pkg.add("PyPlot")
 run(`$(which pip) install -networkunit`)
 valid_test_folder = joinpath(@__DIR__, "testfiles", "valid")
 
